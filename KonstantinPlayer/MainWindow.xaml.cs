9e827b6e7ebc208d0c75a1845353dd828c4ccee9
Ринат Гумиров <rinat.gumirov@mail.ru>
2016-04-22 11:03:37 +0500

    22.04.2016 11:03

--- KonstantinPlayer/MainWindow.xaml.cs
+++ KonstantinPlayer/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace KonstantinPlayer
 {
@@ -35,7 +23,6 @@ namespace KonstantinPlayer
             {
                 string fname = Application.Current.Properties["ArbitraryArgName"].ToString();
                 mediaElement.Source = new Uri(fname);
-
             }
         }
 
@@ -72,6 +59,7 @@ namespace KonstantinPlayer
             object helperObject = hlp.GetValue(myMedia);
             FieldInfo stateField = helperObject.GetType().GetField("_currentState", BindingFlags.NonPublic | BindingFlags.Instance);
             MediaState state = (MediaState)stateField.GetValue(helperObject);
+
             return state;
         }
 
